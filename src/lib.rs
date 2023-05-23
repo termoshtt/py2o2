8b410776be3d9cf7776eb7cbd502303c43adbed6
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-05-23 15:25:41 +0900

    get_interfaces

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,26 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use anyhow::Result;
+use std::collections::HashMap;
+use std::path::Path;
+
+pub fn get_interfaces(path: &Path) -> Result<Vec<wit_parser::Interface>> {
+    let unresolved = wit_parser::UnresolvedPackage::parse_file(path)?;
+    let mut wit = wit_parser::Resolve::new();
+    wit.push(unresolved, &HashMap::new())?;
+    Ok(wit
+        .interfaces
+        .into_iter()
+        .map(|(_id, contents)| contents)
+        .collect())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::collections::HashMap;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 
     #[test]
     fn test_wit_parser() {
         let test_wit = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("test.wit");
-        let unresolved = wit_parser::UnresolvedPackage::parse_file(&test_wit).unwrap();
-        let mut wit = wit_parser::Resolve::new();
-        wit.push(unresolved, &HashMap::new()).unwrap();
-        for (id, contents) in &wit.interfaces {
-            dbg!(id);
-            dbg!(contents);
-        }
-        panic!();
+        let interfaces = get_interfaces(&test_wit).unwrap();
+        dbg!(interfaces);
     }
 }
