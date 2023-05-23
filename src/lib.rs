7a48fae5b3aacf9c41eb5285800b143ba6dff396
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-05-23 14:12:30 +0900

    Test wit_parser

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,24 @@ pub fn add(left: usize, right: usize) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashMap;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_wit_parser() {
+        let test_wit = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("test.wit");
+        let unresolved = wit_parser::UnresolvedPackage::parse_file(&test_wit).unwrap();
+        let mut wit = wit_parser::Resolve::new();
+        wit.push(unresolved, &HashMap::new()).unwrap();
+        for (id, contents) in &wit.interfaces {
+            dbg!(id);
+            dbg!(contents);
+        }
+        panic!();
     }
 }
