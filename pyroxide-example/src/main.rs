dc4e95abc04bdddb018bb09aa2cdc3884d0c391a
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-05-24 13:54:02 +0900

    Moc generated code

--- pyroxide-example/src/main.rs
+++ pyroxide-example/src/main.rs
@@ -1,15 +1,28 @@
 use pyo3::prelude::*;
 
-fn main() -> PyResult<()> {
-    Python::with_gil(|py| {
+pub mod example {
+    use pyo3::prelude::*;
+
+    fn load_pymodule(py: Python<'_>) -> PyResult<&PyModule> {
         // Append current directory `.` to sys.path
         let sys = py.import("sys")?;
         let paths = sys.getattr("path")?;
         paths.call_method("append", ("",), None)?;
 
-        let ex = py.import("example")?;
-        let a1 = ex.getattr("a1")?;
+        py.import("example")
+    }
+
+    pub fn a1(py: Python<'_>) -> PyResult<()> {
+        let m = load_pymodule(py)?;
+        let a1 = m.getattr("a1")?;
         a1.call0()?;
+        Ok(())
+    }
+}
+
+fn main() -> PyResult<()> {
+    Python::with_gil(|py| {
+        example::a1(py)?;
         Ok(())
     })
 }
