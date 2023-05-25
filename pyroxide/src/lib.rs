7174c085648616315699dbde147ec76c708f23b4
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-05-26 00:36:04 +0900

    WIP2: codegen

--- pyroxide/src/lib.rs
+++ pyroxide/src/lib.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TokenStream2;
+use proc_macro2::{Span, TokenStream as TokenStream2};
 use proc_macro_error::*;
 use quote::quote;
 use std::collections::HashMap;
@@ -27,80 +27,46 @@ fn as_rust_type(ty: &wit_parser::Type) -> syn::Type {
     }
 }
 
-fn as_rust_params(params: &[(String, wit_parser::Type)]) -> Vec<(String, syn::Type)> {
-    params
-        .iter()
-        .map(|(name, ty)| (name.clone(), as_rust_type(ty)))
-        .collect()
-}
-
 fn as_rust_tuple(params: &[(String, wit_parser::Type)]) -> syn::Type {
     let params: Vec<syn::Type> = params.iter().map(|(_, ty)| as_rust_type(ty)).collect();
     syn::parse_quote!((#(#params),*))
 }
 
 fn generate_from_wit(wit_path: &Path) -> TokenStream2 {
+    let mut tt = Vec::new();
     let interfaces = get_interfaces(wit_path).unwrap();
     for interface in interfaces {
-        let module_name = &interface.name;
-        for (function_name, f) in interface.functions {
-            dbg!(module_name, function_name);
-            let input = as_rust_params(&f.params);
+        let module_name = interface.name.as_ref().unwrap();
+        let module_ident = syn::Ident::new(module_name, Span::call_site());
+        let mut f_tt = Vec::new();
+        for (name, f) in interface.functions {
+            let ident = syn::Ident::new(&name, Span::call_site());
+            let param_names: Vec<_> = f
+                .params
+                .iter()
+                .map(|(name, _)| syn::Ident::new(name, Span::call_site()))
+                .collect();
+            let param_types: Vec<syn::Type> =
+                f.params.iter().map(|(_, ty)| as_rust_type(ty)).collect();
+            let input_tt = quote!(#(#param_names: #param_types),*);
+
             let output = match &f.results {
                 wit_parser::Results::Named(params) => as_rust_tuple(params),
                 wit_parser::Results::Anon(ty) => as_rust_type(ty),
             };
-            dbg!(input, output);
+            f_tt.push(quote! {
+                pub fn #ident<'py>(py: Python<'py>, #input_tt) -> PyResult<#output> {
+                    todo!()
+                }
+            });
         }
+        tt.push(quote! {
+            pub mod #module_ident {
+                #(#f_tt)*
+            }
+        })
     }
-
-    panic!();
-
-    quote! {
-    pub mod example {
-        use pyo3::{prelude::*, types::PyString};
-
-        pub fn a1(py: Python<'_>) -> PyResult<()> {
-            let _ = py.import("example")?.getattr("a1")?.call((), None)?;
-            Ok(())
-        }
-
-        pub fn a2(py: Python<'_>, x: i64) -> PyResult<()> {
-            let _ = py.import("example")?.getattr("a2")?.call((x,), None)?;
-            Ok(())
-        }
-
-        pub fn a3(py: Python<'_>, y: &str, z: f32) -> PyResult<()> {
-            let _ = py.import("example")?.getattr("a3")?.call((y, z), None)?;
-            Ok(())
-        }
-
-        pub fn a4(py: Python<'_>) -> PyResult<i64> {
-            let result = py.import("example")?.getattr("a4")?.call((), None)?;
-            Ok(result.extract()?)
-        }
-
-        pub fn a5(py: Python<'_>, x: i64) -> PyResult<&PyString> {
-            let result = py.import("example")?.getattr("a5")?.call((x,), None)?;
-            Ok(result.extract()?)
-        }
-
-        pub fn a6(py: Python<'_>) -> PyResult<(i64, &PyString)> {
-            let result = py.import("example")?.getattr("a6")?.call((), None)?;
-            Ok(result.extract()?)
-        }
-
-        pub fn a7(py: Python<'_>, x: i64) -> PyResult<(i64, &PyString, f64)> {
-            let result = py.import("example")?.getattr("a7")?.call((x,), None)?;
-            Ok(result.extract()?)
-        }
-
-        pub fn a8<'py>(py: Python<'py>, x: (i64, &str)) -> PyResult<(i64, &'py PyString, (i64, f64))> {
-            let result = py.import("example")?.getattr("a8")?.call((x,), None)?;
-            Ok(result.extract()?)
-        }
-    }
-    }
+    quote! { #(#tt)* }
 }
 
 fn get_interfaces(path: &Path) -> Result<Vec<wit_parser::Interface>> {
@@ -126,7 +92,9 @@ mod tests {
     }
 
     fn format(tt: TokenStream2) -> String {
-        prettyplease::unparse(&syn::parse_file(&tt.to_string()).unwrap())
+        let input = tt.to_string();
+        eprintln!("{}", input);
+        prettyplease::unparse(&syn::parse_file(&input).unwrap())
     }
 
     #[test]
