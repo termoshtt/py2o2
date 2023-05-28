b87f5e87de9f5738c2456b54f71403f24891ca73
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-05-28 22:05:05 +0900

    impl py2o2

--- pyroxide/src/bin/py2o2.rs
+++ pyroxide/src/bin/py2o2.rs
@@ -1,12 +1,26 @@
+use anyhow::Result;
 use clap::Parser;
-use std::path::PathBuf;
+use std::path::Path;
 
 #[derive(Parser, Debug)]
 struct Cli {
-    path: PathBuf,
+    python_module_name_or_path: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = Cli::parse();
-    dbg!(cli);
+    let path = Path::new(&cli.python_module_name_or_path);
+    let module_name = if path.exists() {
+        std::env::set_var("PYTHONPATH", path.parent().unwrap());
+        let name = path.file_name().unwrap().to_str().unwrap();
+        if let Some(inner) = name.strip_suffix(".py") {
+            inner.to_string()
+        } else {
+            name.to_string()
+        }
+    } else {
+        cli.python_module_name_or_path
+    };
+    println!("{}", pyroxide::generate(&module_name)?);
+    Ok(())
 }
