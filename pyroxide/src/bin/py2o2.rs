209953081a1e5059c3f394d627e7c5fe8f7b8167
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-05-28 20:06:56 +0900

    Setup clap

--- pyroxide/src/bin/py2o2.rs
+++ pyroxide/src/bin/py2o2.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    path: PathBuf,
+}
+
+fn main() {
+    let cli = Cli::parse();
+    dbg!(cli);
+}
