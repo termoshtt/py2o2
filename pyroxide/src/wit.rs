f1a05557fcf4f3aa06cf07583688364e5eca05b9
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-05-30 00:06:32 +0900

    Re-construct tests

--- pyroxide/src/wit.rs
+++ pyroxide/src/wit.rs
@@ -32,37 +32,3 @@ pub fn witgen(target: &str) -> Result<(String, PathBuf)> {
     fs::write(&path, &wit)?;
     Ok((wit, path))
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_wit_parser() {
-        let test_wit = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("../test.wit");
-        let interfaces = parse(&test_wit).unwrap();
-        dbg!(interfaces);
-    }
-
-    #[test]
-    fn test_witgen_example() {
-        // Add a path where `example.py` exists
-        let project_root = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
-            .parent()
-            .unwrap();
-        std::env::set_var("PYTHONPATH", project_root.join("pyroxide-example"));
-
-        let (wit, _path) = witgen("example").unwrap();
-        insta::assert_snapshot!(wit, @r###"
-        interface example {
-        a1: func() 
-        a2: func(x: s64) 
-        a3: func(y: string, z: float64) 
-        a4: func() -> s64
-        a5: func(x: s64) -> string
-        a6: func() -> (out0: s64, out1: string)
-        a7: func(x: s64) -> (out0: s64, out1: string, out2: float64)
-        }
-        "###);
-    }
-}
---
+++ pyroxide/tests/codegen.rs
@@ -0,0 +1,42 @@
+use pyroxide::{codegen, wit};
+use std::path::Path;
+
+#[test]
+fn generate_from_test_wit() {
+    let interfaces =
+        wit::parse(&Path::new(env!("CARGO_MANIFEST_DIR")).join("tests/example.wit")).unwrap();
+    let tt = codegen::generate_from_wit(&interfaces).unwrap();
+    insta::assert_snapshot!(tt, @r###"
+        pub mod example {
+            use pyo3::{prelude::*, types::PyString};
+            pub fn a1<'py>(py: Python<'py>) -> PyResult<()> {
+                let _ = py.import("example")?.getattr("a1")?.call((), None)?;
+                Ok(())
+            }
+            pub fn a2<'py>(py: Python<'py>, x: i64) -> PyResult<()> {
+                let _ = py.import("example")?.getattr("a2")?.call((x,), None)?;
+                Ok(())
+            }
+            pub fn a3<'py>(py: Python<'py>, y: &str, z: f64) -> PyResult<()> {
+                let _ = py.import("example")?.getattr("a3")?.call((y, z), None)?;
+                Ok(())
+            }
+            pub fn a4<'py>(py: Python<'py>) -> PyResult<i64> {
+                let result = py.import("example")?.getattr("a4")?.call((), None)?;
+                Ok(result.extract()?)
+            }
+            pub fn a5<'py>(py: Python<'py>, x: i64) -> PyResult<&'py PyString> {
+                let result = py.import("example")?.getattr("a5")?.call((x,), None)?;
+                Ok(result.extract()?)
+            }
+            pub fn a6<'py>(py: Python<'py>) -> PyResult<(i64, &'py PyString)> {
+                let result = py.import("example")?.getattr("a6")?.call((), None)?;
+                Ok(result.extract()?)
+            }
+            pub fn a7<'py>(py: Python<'py>, x: i64) -> PyResult<(i64, &'py PyString, f64)> {
+                let result = py.import("example")?.getattr("a7")?.call((x,), None)?;
+                Ok(result.extract()?)
+            }
+        }
+        "###);
+}
