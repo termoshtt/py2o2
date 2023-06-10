4c3fbeaafaeae0e9a1ee746151a1ced81e1f8b51
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-06-10 17:51:50 +0900

    Run cargo-fmt after generated

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,14 +1,17 @@
 use anyhow::Result;
-use std::{fs, path::Path};
+use std::{fs, path::Path, process::Command};
 
 const PYTHON_ROOT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../python/");
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     std::env::set_var("PYTHONPATH", PYTHON_ROOT);
     let testing_root = Path::new(env!("CARGO_MANIFEST_DIR")).join("../pyroxide-testing/src");
     for module in ["example", "type_aliases"] {
         let code = pyroxide::generate(module, true)?;
         fs::write(&testing_root.join(format!("{}.rs", module)), code)?;
     }
+
+    let st = Command::new("cargo").arg("fmt").arg("--all").status()?;
+    assert!(st.success());
     Ok(())
 }
