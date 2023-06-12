7d2731681a7d5ffdbc181e998c3a364f0b4f327e
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-06-12 15:58:06 +0900

    Add more cases

--- python/type_aliases.py
+++ python/type_aliases.py
@@ -1,5 +1,18 @@
+# Examples at https://docs.python.org/3/library/typing.html#type-aliases
+
+from collections.abc import Sequence
+
 Vector = list[float]
 
 
 def scale(scalar: float, vector: Vector) -> Vector:
     return [scalar * num for num in vector]
+
+
+ConnectionOptions = dict[str, str]
+Address = tuple[str, int]
+Server = tuple[Address, ConnectionOptions]
+
+
+def broadcast_message(message: str, servers: Sequence[Server]) -> None:
+    pass
