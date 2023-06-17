57c0a831a3c01ce75a625db23b48cb9c3ceabd64
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-06-17 16:54:50 +0900

    Drop old

--- pyroxide/src/lib.rs
+++ pyroxide/src/lib.rs
@@ -1,11 +1,10 @@
-pub mod codegen2;
+pub mod codegen;
 pub mod inspect;
-pub mod wit;
 
 use anyhow::Result;
 
 pub fn generate(python_module_name: &str, bare: bool) -> Result<String> {
     let interface = inspect::Interface::from_py_module(python_module_name)?;
-    let generated = codegen2::generate(python_module_name, &interface, bare)?;
+    let generated = codegen::generate(python_module_name, &interface, bare)?;
     Ok(generated)
 }
