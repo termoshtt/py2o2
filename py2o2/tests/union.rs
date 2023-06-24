e38056eb649fc64e87a1fff749427b7e2cf74ddc
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-06-24 16:50:50 +0900

    Update tests

--- py2o2/tests/union.rs
+++ py2o2/tests/union.rs
@@ -1,12 +1,13 @@
 use anyhow::Result;
-use py2o2::inspect::*;
+use py2o2::{codegen::*, inspect::*};
 
 const PYTHON_ROOT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../python/");
+const TARGET: &str = "union";
 
 #[test]
-fn union() -> Result<()> {
+fn inspect() -> Result<()> {
     std::env::set_var("PYTHONPATH", PYTHON_ROOT);
-    let json = get_inspect_json("union")?;
+    let json = get_inspect_json(TARGET)?;
     insta::assert_snapshot!(json, @r###"
     {
         "functions": {},
@@ -70,5 +71,17 @@ fn union() -> Result<()> {
     }
     "###);
 
+    Ok(())
+}
+
+#[test]
+fn codegen() -> Result<()> {
+    std::env::set_var("PYTHONPATH", PYTHON_ROOT);
+    let interface = Interface::from_py_module(TARGET)?;
+    insta::assert_snapshot!(generate(TARGET, &interface, true)?, @"");
+
+    insta::assert_snapshot!(generate(TARGET, &interface, false)?, @r###"
+    pub mod union {}
+    "###);
     Ok(())
 }
