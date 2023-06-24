c35182797c5503b8c5d067647790a637bd3da9e4
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-06-24 16:43:12 +0900

    Split tests for inspect

---
+++ py2o2/tests/union.rs
@@ -0,0 +1,74 @@
+use anyhow::Result;
+use py2o2::inspect::*;
+
+const PYTHON_ROOT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../python/");
+
+#[test]
+fn union() -> Result<()> {
+    std::env::set_var("PYTHONPATH", PYTHON_ROOT);
+    let json = get_inspect_json("union")?;
+    insta::assert_snapshot!(json, @r###"
+    {
+        "functions": {},
+        "type_definitions": {},
+        "unions": {
+            "union_new": {
+                "args": [
+                    {
+                        "kind": "primitive",
+                        "name": "int"
+                    },
+                    {
+                        "kind": "primitive",
+                        "name": "str"
+                    }
+                ]
+            },
+            "union_old": {
+                "args": [
+                    {
+                        "kind": "primitive",
+                        "name": "int"
+                    },
+                    {
+                        "kind": "primitive",
+                        "name": "str"
+                    }
+                ]
+            }
+        }
+    }
+    "###);
+
+    let interface = Interface::from_json(&json)?;
+    insta::assert_debug_snapshot!(interface, @r###"
+    Interface {
+        functions: {},
+        type_definitions: {},
+        unions: {
+            "union_new": Union {
+                args: [
+                    Primitive(
+                        Int,
+                    ),
+                    Primitive(
+                        Str,
+                    ),
+                ],
+            },
+            "union_old": Union {
+                args: [
+                    Primitive(
+                        Int,
+                    ),
+                    Primitive(
+                        Str,
+                    ),
+                ],
+            },
+        },
+    }
+    "###);
+
+    Ok(())
+}
