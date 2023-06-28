92ab3289712398a3c3362ae203086caa9cc8b0d8
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-06-29 01:34:08 +0900

    Generate Enum3

--- py2o2-runtime/src/lib.rs
+++ py2o2-runtime/src/lib.rs
@@ -12,55 +12,43 @@ pub trait AsPyType {
     fn is_type_of(obj: &PyAny) -> bool;
 }
 
-impl AsPyType for i32 {
-    fn is_type_of(obj: &PyAny) -> bool {
-        PyLong::is_type_of(obj)
-    }
-}
-impl AsPyType for i64 {
-    fn is_type_of(obj: &PyAny) -> bool {
-        PyLong::is_type_of(obj)
-    }
-}
-
-impl AsPyType for f32 {
-    fn is_type_of(obj: &PyAny) -> bool {
-        PyFloat::is_type_of(obj)
-    }
-}
-impl AsPyType for f64 {
-    fn is_type_of(obj: &PyAny) -> bool {
-        PyFloat::is_type_of(obj)
-    }
-}
-
-impl AsPyType for &PyString {
-    fn is_type_of(obj: &PyAny) -> bool {
-        PyString::is_type_of(obj)
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum Enum2<T1, T2> {
-    Item1(T1),
-    Item2(T2),
-}
-
-impl<'s, T1, T2> FromPyObject<'s> for Enum2<T1, T2>
-where
-    T1: AsPyType + FromPyObject<'s>,
-    T2: AsPyType + FromPyObject<'s>,
-{
-    fn extract(ob: &'s PyAny) -> PyResult<Self> {
-        if T1::is_type_of(ob) {
-            return Ok(Enum2::Item1(ob.extract()?));
+macro_rules! impl_as_py_type {
+    ($ty:ty, $pyty:ty) => {
+        impl AsPyType for $ty {
+            fn is_type_of(obj: &PyAny) -> bool {
+                <$pyty>::is_type_of(obj)
+            }
         }
-        if T2::is_type_of(ob) {
-            return Ok(Enum2::Item2(ob.extract()?));
-        }
-        Err(PyTypeError::new_err("Type mismatch"))
-    }
+    };
 }
+impl_as_py_type!(i64, PyLong);
+impl_as_py_type!(f64, PyFloat);
+impl_as_py_type!(&PyString, PyString);
+
+macro_rules! define_enum {
+    ($enum:ident; $($item:ident),* ; $($t:ident),*) => {
+        #[derive(Debug, PartialEq, Clone)]
+        pub enum $enum<$($t),*> {
+            $($item($t)),*
+        }
+
+        impl<'s, $($t),*> FromPyObject<'s> for $enum<$($t),*>
+        where
+            $($t: AsPyType + FromPyObject<'s>),*
+        {
+            fn extract(ob: &'s PyAny) -> PyResult<Self> {
+                $(
+                if $t::is_type_of(ob) {
+                    return Ok($enum::$item(ob.extract()?));
+                }
+                )*
+                Err(PyTypeError::new_err("Type mismatch"))
+            }
+        }
+    };
+}
+define_enum!(Enum2; Item1, Item2; T1, T2);
+define_enum!(Enum3; Item1, Item2, Item3; T1, T2, T3);
 
 #[cfg(test)]
 mod tests {
@@ -71,19 +59,19 @@ mod tests {
     #[test]
     fn convert() -> Result<()> {
         Python::with_gil(|py| -> Result<()> {
-            let v1: i32 = 42;
-            let v2: f32 = 2.123;
+            let v1: i64 = 42;
+            let v2: f64 = 2.123;
 
             let p1: Py<PyAny> = v1.into_py(py);
-            let e: Enum2<i32, f32> = p1.extract(py)?;
+            let e: Enum2<i64, f64> = p1.extract(py)?;
             assert_eq!(e, Enum2::Item1(v1));
 
             let p2: Py<PyAny> = v2.into_py(py);
-            let e: Enum2<i32, f32> = p2.extract(py)?;
+            let e: Enum2<i64, f64> = p2.extract(py)?;
             assert_eq!(e, Enum2::Item2(v2));
 
             let p3: Py<PyAny> = "test".into_py(py);
-            assert!(p3.extract::<Enum2<i32, f32>>(py).is_err());
+            assert!(p3.extract::<Enum2<i64, f64>>(py).is_err());
 
             Ok(())
         })?;
