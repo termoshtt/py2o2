864e6ec59c077459dfd457fa3ed1b7e4654ba73a
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-07-01 13:57:47 +0900

    as_pycfunc

--- py2o2-testing/src/lib.rs
+++ py2o2-testing/src/lib.rs
@@ -69,35 +69,49 @@ fn caller<'py>(py: Python<'py>, f: &'py PyCFunction) -> PyResult<()> {
     Ok(())
 }
 
+fn as_pycfunc0<'py, F>(py: Python<'py>, f: F) -> PyResult<&'py PyCFunction>
+where
+    F: Fn() -> Py<PyString> + Send + 'static,
+{
+    PyCFunction::new_closure(
+        py,
+        None,
+        None,
+        move |_args: &PyTuple, _kwargs: Option<&PyDict>| -> PyResult<_> { Ok(f()) },
+    )
+}
+
+fn as_pycfunc1<'py, F>(py: Python<'py>, f: F) -> PyResult<&'py PyCFunction>
+where
+    F: Fn(i64, f64) -> f64 + Send + 'static,
+{
+    PyCFunction::new_closure(
+        py,
+        None,
+        None,
+        move |args: &PyTuple, _kwargs: Option<&PyDict>| -> PyResult<_> {
+            let (a, b): (i64, f64) = args.extract()?;
+            let out = f(a, b);
+            Ok(out)
+        },
+    )
+}
+
 #[test]
 fn callable() -> Result<()> {
     std::env::set_var("PYTHONPATH", PYTHON_ROOT);
     Python::with_gil(|py| {
-        let f = PyCFunction::new_closure(
-            py,
-            None,
-            None,
-            |_args: &PyTuple, _kwargs: Option<&PyDict>| -> PyResult<Py<PyString>> {
-                static mut COUNT: usize = 0;
-                let current = unsafe {
-                    COUNT += 1;
-                    COUNT
-                };
-                let s = Python::with_gil(|py2| PyString::new(py2, &format!("{}", current)).into());
-                Ok(s)
-            },
-        )?;
+        let f = as_pycfunc0(py, || {
+            static mut COUNT: usize = 0;
+            let current = unsafe {
+                COUNT += 1;
+                COUNT
+            };
+            Python::with_gil(|py2| PyString::new(py2, &format!("{}", current)).into())
+        })?;
         feeder(py, f)?;
 
-        let g = PyCFunction::new_closure(
-            py,
-            None,
-            None,
-            |args: &PyTuple, _kwargs: Option<&PyDict>| -> PyResult<f64> {
-                let (a, b): (i64, f64) = args.extract()?;
-                Ok(a as f64 * b)
-            },
-        )?;
+        let g = as_pycfunc1(py, |a: i64, b: f64| a as f64 * b)?;
         caller(py, g)?;
 
         Ok(())
